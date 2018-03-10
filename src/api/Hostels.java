5bf36257f3a704c6c5d5a66a87100d1ea19a83cb
Pierre cl <pc.clochard@gmail.com>
2018-03-10 20:49:25 +0100

    first parameter approch

--- src/api/Hostels.java
+++ src/api/Hostels.java
@@ -2,6 +2,7 @@ package api;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.GET;
 import javax.ws.rs.ApplicationPath;
@@ -9,16 +10,26 @@ import javax.ws.rs.core.Application;
 
 @Path("/hostels")
 public class Hostels {
+	//GET method, should return the list of all hostels as JSON
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String displayHostelsJSON(){
+	public String displayHostelsJSON(@QueryParam("name") String name){
+		if(!name.isEmpty()){
+			
+		}
 		String resource = null;
 		return resource;
 	}
+	//GET simple method to catch HTML access
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String displayHostelsHTML(){
-		String resource = "<h1>hostels<h1>";
+	public String displayHostelsHTML(@QueryParam("name") String name){
+		String resource = "";
+		if(!name.isEmpty()){
+			
+		 resource = "<h3>hostels "+ name +"<h3>";
+
+		}
 		return resource;
 	}
 	
